fdae58b99494a085d2c57d1af79ba780ca3362e0
Digiwave456 <hot.ershik77@gmail.com>
2025-06-22 21:52:47 +0400

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-@extends('Layouts.app')
+@extends('layouts.app')
 @section('content')
 
 <!-- Баннер акции -->
