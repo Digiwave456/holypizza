f8ecf0347f41849908b0f23a06eb3c15435c3a4c
Digiwave456 <hot.ershik77@gmail.com>
2025-06-22 22:34:52 +0400

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -31,7 +32,7 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
-    <footer class="footer">
+    <footer class="footer" style="position: fixed; left: 0; bottom: 0; width: 100%;">
         <div class="container footer-container">
             <div class="footer-section">
                 
@@ -51,7 +52,6 @@
                 <h4>Наши соцсети</h4>
                 <div class="social-links">
                     <a href="#" title="ВКонтакте"><i class="fab fa-vk"></i></a>
-                    <a href="#" title="YouTube"><i class="fab fa-youtube"></i></a>
                     <a href="#" title="Telegram"><i class="fab fa-telegram"></i></a>
                 </div>
                 <p>@holypizza</p>
